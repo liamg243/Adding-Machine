2c3d90dd441df20ea129f11e95e83bf0f5d93c19
Liam G <lgomez19@asu.edu>
2020-10-07 22:16:01 -0700

    version 2 with the functional adding machine code

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,30 +1,40 @@
+/* Class: CSE 360
+ * Author: Liam Gomez
+ * ASU ID: 1214820607
+ */
 package cse360assignment02;
 
 public class AddingMachine {
 	
 	private int total;
+	private String totalChar = "0";	
 	
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
 	}
 	
 	public int getTotal() {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		totalChar = totalChar + " + ";
+		totalChar = totalChar + value;
 	}
 	
-	public void subtract (int valut) {
-		
+	public void subtract (int value) {
+		total = total - value;
+		totalChar = totalChar + " - ";
+		totalChar = totalChar + value;
 	}
 	
 	public String toString() {
-		return "";
+		return totalChar;
 	}
 	
 	public void clear() {
-		
+		total = 0;
+		totalChar = "0";
 	}
 }
